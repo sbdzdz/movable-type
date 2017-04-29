6ce5a76748d2268c7180af0fa32f7112d0cfa1e6
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2017-04-30 00:58:18 +0200

    Try t-SNE

--- tfidf.py
+++ tfidf.py
@@ -1,6 +1,10 @@
 import glob
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.manifold import TSNE
 
 def main():
     pattern = './corpus/*.txt'
@@ -11,6 +15,17 @@ def main():
     similarity = cosine_similarity(tfidf_matrix)
     for index, file in enumerate(files):
         print("{}: {}".format(file, (list(similarity[index]))))
+
+    dense_tfidf_matrix = tfidf_matrix.toarray()
+    model = TSNE(n_components=2, random_state=0, metric='cosine', learning_rate=150)
+    points = model.fit_transform(dense_tfidf_matrix)
+
+    df = pd.DataFrame(points, index=files, columns=['x', 'y'])
+    fig, ax = plt.subplots()
+    df.plot('x', 'y', kind='scatter', ax=ax)
+    for k, v in df.iterrows():
+        ax.annotate(k, v)
+    plt.show()
 
 if __name__ == '__main__':
     main()
