2566ab6df1c54d914f3bb3e4e9c26be4bff9a6ba
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2017-04-29 22:52:58 +0200

    Remove unused code, improve naming, use files as inputs

--- tfidf.py
+++ tfidf.py
@@ -1,27 +1,16 @@
-import os
-import pandas as pd
-import numpy as np
+import glob
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from nltk.corpus import gutenberg
 
 def main():
-    path = './corpus'
-    titles, corpus = [], []
-    for filename in os.listdir(path):
-        file_path = os.path.join(path, filename)
-        titles.append(filename)
-        with open(file_path, 'r') as f:
-            corpus.append(f.read().replace('\n', ''))
-    titles = gutenberg.fileids()
-    corpus = [gutenberg.raw(title) for title in titles]
-    vectorizer = TfidfVectorizer(max_df=.9, stop_words='english')
-    weights = vectorizer.fit_transform(corpus).toarray()
+    pattern = './corpus/*.txt'
+    files = glob.glob(pattern)
+    vectorizer = TfidfVectorizer(input='filename', max_df=.9, stop_words='english')
+    tfidf_matrix = vectorizer.fit_transform(files).toarray()
 
-    similarity = cosine_similarity(weights)
-    most_similar = np.argmax(similarity, axis=0)
-    for index, title in enumerate(titles):
-        print("{}: {}".format(title, (list(similarity[index]))))
+    similarity = cosine_similarity(tfidf_matrix)
+    for index, file in enumerate(files):
+        print("{}: {}".format(file, (list(similarity[index]))))
 
 if __name__ == '__main__':
     main()
