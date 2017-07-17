ddf26dd266763b279da93ffd2411edffa709411a
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2017-07-17 12:22:42 +0200

    Implement tf-idf the same way as in the blog post

--- tfidf_example.py
+++ tfidf_example.py
@@ -3,17 +3,14 @@ from math import log
 
 doc1 = 'egg bacon sausage and spam'
 doc2 = 'spam bacon sausage and spam'
-doc3 = 'spam spam spam baked beans and spam'
-corpus = {doc1, doc2, doc3}
+doc3 = 'spam spam spam spam spam spam baked beans spam spam spam and spam'
+D = {doc1, doc2, doc3}
 
-count = Counter(doc1.split())
+count = Counter(doc2.split())
 terms = count.keys()
 total = sum(count.values())
-tf_idf = {}
 
-for term in terms:
-    tf = count[term]/total
-    idf = log(len(corpus)/(sum(term in document.split() for document in corpus)))
-    tf_idf[term] = tf*idf
-
+tf = {t: count[t]/total for t in terms}
+idf = {t: log(len(D)/sum(t in d.split() for d in D)) for t in terms}
+tf_idf = {t: round(tf[t]*idf[t], 3) for t in terms}
 print(tf_idf)
